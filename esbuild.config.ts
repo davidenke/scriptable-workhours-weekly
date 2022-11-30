3f246416deda2823d43ea6e163944dca9ece0fc2
David Enke <david@enke.dev>
2022-11-30 11:00:50 +0100

    init

---
+++ esbuild.config.ts
@@ -0,0 +1,127 @@
+import { copyFile, readFile, writeFile } from 'fs/promises';
+import { homedir } from 'os';
+import { dirname, resolve } from 'path';
+
+import { build, type Plugin } from 'esbuild';
+import { clean } from 'esbuild-plugin-clean';
+import { merge as _merge } from 'lodash-es';
+
+import * as pkg from './package.json' assert { type: 'json' };
+
+const isWatchMode = process.argv.includes('--watch');
+
+type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]> } : T;
+
+type Scriptable = {
+  always_run_in_app: boolean;
+  icon: {
+    color: string;
+    glyph: string;
+  };
+  name: string;
+  script: string;
+  share_sheet_inputs: string[];
+};
+
+type ScriptableOptions = {
+  scriptfile: string;
+  outfile?: string;
+  package?: string | DeepPartial<Scriptable>;
+};
+
+const prepareScriptable = (options: ScriptableOptions): Plugin => ({
+  name: 'esbuild-plugin-scriptable',
+  setup(build) {
+    build.onEnd(async results => {
+      // do not go on if there are errors
+      if (results.errors.length > 0) return;
+
+      // prepare target
+      const scriptName = (typeof options.package !== 'string' && options.package?.name) ?? pkg.name;
+      let { outdir, outfile } = build.initialOptions;
+      let target: string;
+      if (options.outfile) {
+        target = resolve(options.outfile);
+      } else if (outdir) {
+        target = resolve(outdir, `${scriptName}.scriptable`);
+      } else if (outfile) {
+        target = resolve(dirname(outfile), `${scriptName}.scriptable`);
+      } else {
+        throw new Error('No output file could be determined');
+      }
+
+      // prepare package
+      const script = await readFile(resolve(options.scriptfile), 'utf-8');
+      const packageDefaults: Scriptable = {
+        always_run_in_app: false,
+        icon: {
+          color: 'deep-blue',
+          glyph: 'magic'
+        },
+        name: pkg.name,
+        script,
+        share_sheet_inputs: []
+      };
+
+      // given package data can be set inline or as file
+      let packageData: DeepPartial<Scriptable> | undefined;
+      if (options.package) {
+        // assume a file path
+        if (typeof options.package === 'string') {
+          const rawPackageData = await readFile(resolve(options.package), 'utf-8');
+          packageData = JSON.parse(`${rawPackageData}`);
+        }
+        // assume inline data
+        else {
+          packageData = options.package;
+        }
+      }
+      const scriptable: Scriptable = _merge({}, packageDefaults, packageData);
+
+      // write package to target
+      return writeFile(target, JSON.stringify(scriptable, null, 2));
+    });
+  }
+});
+
+build({
+  entryPoints: ['./src/index.ts'],
+  outfile: './dist/index.js',
+  bundle: true,
+  platform: 'node',
+  format: 'esm',
+  minify: !isWatchMode,
+  sourcemap: isWatchMode,
+  watch: isWatchMode,
+  logLevel: 'info',
+  plugins: [
+    clean({ patterns: ['./dist'] }),
+    prepareScriptable({
+      scriptfile: './dist/index.js',
+      package: {
+        always_run_in_app: true,
+        name: 'Workhours Weekly',
+        icon: {
+          color: 'blue',
+          glyph: 'business-time'
+        }
+      }
+    }),
+    {
+      name: 'Copy target',
+      setup(build) {
+        build.onEnd(() => {
+          copyFile(
+            resolve('./dist/index.js'),
+            resolve(
+              homedir(),
+              './Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/Workhours Weekly.js'
+            )
+          );
+        });
+      }
+    }
+  ]
+})
+  .then(() => !isWatchMode && process.exit(0))
+  .catch(() => process.exit(1));
---
+++ src/index.ts
@@ -0,0 +1,125 @@
+type WidgetParameters = {
+  context: string;
+  token: string;
+};
+
+const readParameters = (): WidgetParameters => {
+  const { context = 'context', token = 'token' } = JSON.parse(args.widgetParameter ?? '{}');
+  return { context, token };
+};
+
+const createWidget = async (context: string, token: string): Promise<ListWidget> => {
+  const gradient = new LinearGradient();
+  gradient.locations = [0, 1];
+  gradient.colors = [new Color('#141414', 1), new Color('#13233F', 1)];
+
+  const widget = new ListWidget();
+  widget.backgroundGradient = gradient;
+  widget.useDefaultPadding();
+
+  const wrapper = widget.addStack();
+  wrapper.layoutHorizontally();
+
+  wrapper.addImage(createProgressCircle(0.6).getImage());
+  wrapper.addSpacer(18);
+
+  const infos = wrapper.addStack();
+  infos.addText(context ?? '');
+  infos.addSpacer(18);
+  infos.addText(token ?? '');
+
+  return widget;
+};
+
+const getPercentageColor = (percentage: number): Color => {
+  const colors = [
+    new Color('#ec5858', 1),
+    new Color('#f69153', 1),
+    new Color('#ffca4d', 1),
+    new Color('#e0d84f', 1),
+    new Color('#c0e650', 1)
+  ];
+  return colors[Math.floor((colors.length - 1) * percentage)];
+};
+
+const createProgressCircle = (percentage: number, size = 100, stroke = 10): DrawContext => {
+  const context = new DrawContext();
+  context.opaque = false;
+  context.respectScreenScale = true;
+  context.size = new Size(size, size);
+
+  // draw background circle
+  context.setLineWidth(stroke);
+  context.setStrokeColor(new Color('#6fbad9', 0.3));
+  context.strokeEllipse(new Rect(stroke / 2, stroke / 2, size - stroke, size - stroke));
+
+  const color = getPercentageColor(percentage);
+  context.setStrokeColor(color);
+
+  // draw circle segment
+  const center = new Point(size / 2, size / 2);
+  const radius = (size - stroke) / 2;
+
+  const getPointOnCircle = (degrees: number): Point => {
+    var radians = ((degrees - 90) * Math.PI) / 180;
+    const x = center.x + radius * Math.cos(radians);
+    const y = center.y + radius * Math.sin(radians);
+    return new Point(x, y);
+  };
+
+  const drawArc = (from: number, to: number): Path => {
+    const start = getPointOnCircle(360 * from);
+    const mid = getPointOnCircle(360 * (to - from)); // @todo: calc control point instead of mid point
+    const end = getPointOnCircle(360 * Math.max(to, from + 0.25));
+    const segment = new Path();
+    segment.move(start);
+    segment.addQuadCurve(end, mid);
+    return segment;
+  };
+
+  // calculate amount of full path segments
+  const arcs = percentage / 0.25;
+  const length = Math.floor(arcs);
+
+  // draw full segments
+  Array.from({ length }, (_, index) => {
+    context.addPath(drawArc(0.25 * index, 0.25 * index + 0.25));
+  });
+
+  // draw partial segment
+  if (length - arcs > 0) {
+    context.addPath(drawArc(length * 0.25, length * 0.25 + length - arcs));
+  }
+
+  context.strokePath();
+  return context;
+};
+
+const { context, token } = readParameters();
+const widget = await createWidget(context, token);
+
+if (!config.runsInWidget) {
+  widget.presentMedium();
+} else {
+  Script.setWidget(widget);
+}
+
+Script.complete();
+
+export {};
+// ^^^ This export statement will be omitted in the final bundle
+
+// if (config.runsInWidget) {
+//   // Tell the widget on the Home Screen to show our ListWidget instance.
+//   const widget = await createWidget(items);
+//   Script.setWidget(widget);
+// } else if (config.runsWithSiri) {
+//   // Present a table with a subset of the news.
+//   let firstItems = items.slice(0, 5);
+//   // let table = createTable(firstItems);
+//   // await QuickLook.present(table);
+// } else {
+//   // Present the full list of news.
+//   // let table = createTable(items);
+//   // await QuickLook.present(table);
+// }
